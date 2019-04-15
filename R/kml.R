195703407d71e9a38351d1e4ca0b4aa8ffd197f9
Sam Son <sosson97@gmail.com>
2019-04-16 02:12:58 +0900

    kml.R now returns full matrix

--- R/kml.R
+++ R/kml.R
@@ -1,10 +1,15 @@
 ###1. Load csv file as a matrix in R
-dat <- read.csv(file.choose())
+library(kml)
+num <- 7
+cluster_num <- 15
+infile_name <- "/home/sam95/CD3/simple/raw/players_until_career_7.csv" 
+outfile_name <- "/home/sam95/CD3/simple/raw/clusters_players_until_career_7.csv"
+dat <- read.csv(infile_name)
 mat <- as.matrix(dat)
  
 ###2. Extract WAR by time data from matrix
 ###Note: mat[, 3:3+(n-1)] should be used when using career_until_n_years.csv
-wmat <- mat[, 3:6]
+wmat <- mat[, 3:(2+num)]
 class(wmat) <- "numeric"
  
 ###3. Create clusterLondData from matrix
@@ -12,8 +17,13 @@ ld <- clusterLongData(wmat)
  
 ###4. Execute kml algorithm then get clustering result
 ###Note: 2nd argument to kml function determines how many clusters you use.
-kml(ld, 10:15)
-out <- getClusters(ld,15)
- 
+kml(ld, cluster_num)
+out <- getClusters(ld,cluster_num)
+out_mat <- as.matrix(out)
+colnames(out_mat) <- c("Cluster")
+output <- cbind(cbind(mat[, 1:2],wmat), out_mat)
+
+
+
 ###5. Dump the result
-write.csv(out, file=fileName)
+write.csv(output, file=outfile_name, row.names=FALSE)
